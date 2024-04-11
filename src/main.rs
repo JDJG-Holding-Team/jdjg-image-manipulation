fb369e28a4911a20bc4402ac9f8706b1e9c1afac
JDJG Inc. Official <ghostzap123@gmail.com>
2024-04-10 21:14:09 -0400

    Wish me Luck i guess?
    Man rust will be difficult.

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use std::io::Cursor;
+use image::io::Reader as ImageReader;
+
+
+fn main() {
+    // https://docs.rs/image/latest/image/
+}
+
+fn crusty(){
+    //idk about the type needed to be passed in
+    // any error will return the og.
+}
