98ec35881b87fdc8bf2f1d74882c57e0cc7bd29a
JDJG <ghostzap123@gmail.com>
2024-04-10 21:19:08 -0400

    there

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,47 @@
 use std::io::Cursor;
 use image::io::Reader as ImageReader;
 
-
-fn main() {
+fn main(){
+    
+    //crusty();
     // https://docs.rs/image/latest/image/
+    
+    // will likely need to take in bytes
+    
+    /*
+    
+    def crusty(raw_assets: bytes) -> discord.File:
+    f = BytesIO()
+
+    with WImage(blob=raw_assets) as img:
+        if img.format in ("GIF",):
+            img.coalesce()
+            img.iterator_reset()
+
+        for d in (35, 500):
+            img.resize(d, d)
+
+        img.save(file=f)
+        ext = img.format
+
+    f.seek(0)
+    return discord.File(f, f"crusty.{ext}")
+    
+    */
+    
+    //goal rewrite that from python to rust will likely only return to bytes though
+    
 }
 
-fn crusty(){
-    //idk about the type needed to be passed in
-    // any error will return the og.
+fn crusty(image_bytes : u128) -> u128{
+    const LOWEST_SIZE: i32 = 32;
+    const HIGHEST_SIZE: i32 = 500;
+    
+    println!("{}", LOWEST_SIZE);
+    println!("{}", HIGHEST_SIZE);
+    
+    return image_bytes;
+    
+    //will be changed after this point.
+    //idk if pass arg is right.
 }
