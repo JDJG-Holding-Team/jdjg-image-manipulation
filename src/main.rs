cf8a33999f98e038c4a0b5920942bb72014cbc9f
JDJG <ghostzap123@gmail.com>
2024-04-16 04:15:36 -0400

    There

--- src/main.rs
+++ src/main.rs
@@ -80,8 +80,8 @@ fn gadget(text: String) -> Vec<u8> {
 fn invert(image_bytes: Vec<u8>) -> Vec<u8> {
     // invert bytes and keep gifs as gifs, and other content the same etc.
 
-    // make sure to crate bytes to save to or save to new image_bytes.
-    let img = ImageReader::new(Cursor::new(image_bytes.clone())).with_guessed_format().unwrap().decode().unwrap();
+    
+    let mut img = ImageReader::new(Cursor::new(image_bytes.clone())).with_guessed_format().unwrap().decode().unwrap();
     img.invert();
 
     // idk about inverting????
