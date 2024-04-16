0a4eb1cd50d459741b4b360cdd4f33ad3f9f37e3
JDJG <ghostzap123@gmail.com>
2024-04-16 04:04:07 -0400

    this pains me

--- src/main.rs
+++ src/main.rs
@@ -64,7 +64,7 @@ fn gadget(text: String) -> Vec<u8> {
 
     let mut final_bytes: Vec<u8> = Vec::new();
     // let error_img = ImageReader::open("/assets/images/bad_output.png")?.decode()?;
-    let gadget_img = ImageReader::open("/assets/images/gadget.png")?.decode().unwrap();
+    let gadget_img = ImageReader::open("/assets/images/gadget.png").unwrap().decode().unwrap();
 
     // gadget_img.write_to(&mut Cursor::new(&mut final_bytes));
     // how do I get the ImageFormat for it?
@@ -80,7 +80,7 @@ fn gadget(text: String) -> Vec<u8> {
 fn invert(image_bytes: Vec<u8>) -> Vec<u8> {
     // invert bytes and keep gifs as gifs, and other content the same etc.
 
-    let img = ImageReader::new(Cursor::new(image_bytes)).with_guessed_format()?.decode().unwrap(); 
+    let img = ImageReader::new(Cursor::new(image_bytes)).with_guessed_format().unwrap().decode().unwrap(); 
     // idk about inverting????
 
     return image_bytes;
@@ -92,7 +92,7 @@ fn invert(image_bytes: Vec<u8>) -> Vec<u8> {
 fn call_text(text: String) -> Vec<u8> {
 
     let mut final_bytes: Vec<u8> = Vec::new();
-    let call_img = ImageReader::open("/assets/images/calling_template.png")?.decode().unwrap();
+    let call_img = ImageReader::open("/assets/images/calling_template.png").unwrap().decode().unwrap();
 
     // call_img.write_to(&mut Cursor::new(&mut final_bytes));
     // how do I get the ImageFormat?
